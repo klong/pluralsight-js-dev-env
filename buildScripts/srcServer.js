c3bfe27163f76c72e0ab3fcecf84e1c3acdd91c3
karl longman <karl_longman@hotmail.com>
2016-11-29 12:12:01 +0000

    mockAPI

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -15,6 +15,15 @@ app.use(require('webpack-dev-middleware')(compiler, {
   publicPath: config.output.publicPath
 }));
 
+app.get('/users', function (req, res) {
+  // hardcoded JSON data pretending to be a database result
+  res.json([
+    {"id": "1", "firstname": "Bob", "lastname": "Smith", "email": "bobgob@gmail.com"},
+    {"id": "2", "firstname": "Tammu", "lastname": "Norton", "email": "tam@gmail.com"},
+    {"id": "3", "firstname": "Tina", "lastname": "Lee", "email": "tina.lee@hotmail.com"},
+  ]);
+});
+
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'))
 });
